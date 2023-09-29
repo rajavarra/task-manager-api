847f1148b51a6c39c8c37ba43908f96fe9a1bc42
rajavarra <rajamohan.varra@gmail.com>
2023-09-29 15:55:49 +0530

    added post put and delete methods

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,5 +1,5 @@
 const { validate: uuidValidate } = require('uuid');
-
+const { customBodyValidator } = require('../database/utils');
 const taskService = require("../services/taskService");
 
 const getAllTasks = (req, res) => {
@@ -28,7 +28,62 @@ const getOneTask = (req, res) => {
     }
 }
 
+const createNewtask = (req, res) => {
+    const { body } = req;
+    // set task prioprity to low if user not provides the value
+    const priority = body.priority || "low";
+    const newTask = {
+        title: body.title,
+        description: body.description,
+        completed: body.completed,
+        priority: priority,
+    }
+    try {
+        customBodyValidator(newTask);
+        const createdTask = taskService.createNewTask(newTask);
+        res.status(201).send(createdTask);
+    } catch (error) {
+        res.status(error?.status || 500).send({ error: error.message || error });
+    }
+}
+
+const updateOneTask = (req, res) => {
+    const { body, params: { taskId } } = req;
+    if (!uuidValidate(taskId)) {
+        return res.status(400).send({ error: "Invalid task ID parameter" });
+    }
+
+    const updateTask = {
+        title: body.title,
+        description: body.description,
+        completed: body.completed,
+        priority: body.priority,
+    }
+    try {
+        customBodyValidator(updateTask);
+        const updatedTask = taskService.updateOneTask(taskId, updateTask);
+        res.status(201).send(updatedTask);
+    } catch (error) {
+        res.status(error?.status || 500).send({ error: error?.message || error });
+    }
+};
+
+const deleteOneTask = (req, res) => {
+    const { params: { taskId } } = req;
+    if (!uuidValidate(taskId)) {
+        return res.status(400).send({ error: "Invalid task ID parameter" });
+    }
+    try {
+        taskService.deleteOneTask(taskId);
+        res.status(204).send({});
+    } catch (error) {
+        res.status(error?.status || 500).send({ error: error?.message || error });
+    }
+};
 module.exports = {
     getAllTasks,
     getOneTask,
+    createNewtask,
+    updateOneTask,
+    deleteOneTask,
 }
--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,4 +1,5 @@
 const Task = require("../database/Task");
+const { v4: uuid } = require('uuid');
 
 const getAllTasks = () => {
     const allTasks = Task.getAllTasks();
@@ -9,8 +10,42 @@ const getOneTask = (taskId) => {
     const task = Task.getOneTask(taskId);
     return task;
 }
+const createNewTask = (newTask) => {
+
+    const task = {
+        ...newTask,
+        id: uuid(),
+    }
+    try {
+        const createdTask = Task.createNewTask(task);
+        return createdTask;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const updateOneTask = (taskId, taskBodyToUpdate) => {
+    try {
+        const updatedTask = Task.updateOneTask(taskId, taskBodyToUpdate);
+        return updatedTask;
+    } catch (error) {
+        throw error;
+    }
+
+};
+
+const deleteOneTask = (taskId) => {
+    try {
+        Task.deleteOneTask(taskId);
+    } catch (error) {
+        throw error;
+    }
+};
 
 module.exports = {
     getAllTasks,
     getOneTask,
+    createNewTask,
+    updateOneTask,
+    deleteOneTask,
 }
